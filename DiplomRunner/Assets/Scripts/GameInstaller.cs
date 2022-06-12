75f4f8acc11e40658b1011815210f45a12e78a41
Pyvel24 <goloveyan@mail.ru>
2022-06-13 00:31:07 +0300

    create script GameInstaller

---
+++ DiplomRunner/Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace
+{
+    public class GameInstaller: MonoInstaller
+    {
+        [SerializeField] private LevelView levelViewPrefab;
+        
+        public override void InstallBindings()
+        {
+            Container.BindMemoryPool<LevelView, LevelView.Pool>()
+                .WithInitialSize(2)
+                .FromNewComponentOnNewPrefab(levelViewPrefab);
+
+            Container.BindInterfacesTo<Test>().AsSingle().NonLazy();
+        }
+        
+        
+    }
+}
