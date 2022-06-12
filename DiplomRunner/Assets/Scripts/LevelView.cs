dfae6fd0c2b18e34275538abf7316d879594a3c0
Pyvel24 <goloveyan@mail.ru>
2022-06-13 00:31:26 +0300

    create script LevelView

---
+++ DiplomRunner/Assets/Scripts/LevelView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class LevelView : MonoBehaviour
+{
+    public class Pool : MonoMemoryPool<Vector3, LevelView>
+    {
+        protected override void Reinitialize(Vector3 p1, LevelView item)
+        {
+            item.transform.position = p1;
+        }
+    }
+}
