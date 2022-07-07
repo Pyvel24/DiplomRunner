f9c8ec3490c4c3aad9a0b9e528ac4006946ed892
Pyvel24 <goloveyan@mail.ru>
2022-07-07 19:20:12 +0300

    Update Test.cs

--- DiplomRunner/Assets/Scripts/Test.cs
+++ DiplomRunner/Assets/Scripts/Test.cs
@@ -1,37 +1,36 @@
 ﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 using Zenject;
 
 namespace DefaultNamespace
 {
-    public class Test : ITickable
+    public class Test
     {
-        private int offset = 31;
-        private float speed = 0.5f;
+        private int offset = 30;
+        private float speed = 0.1f;
         private readonly LevelView.Pool _pool;
-        private int counter = 2;
+        private int _counter = 1;
         private Queue<LevelView> _queue;
-
+ 
         public Test(LevelView.Pool pool)
         {
             _pool = pool;
             _queue = new Queue<LevelView>();
             _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, 0)));
             _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, offset)));
-            _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, offset * counter)));
-
+            Drug();
         }
 
-        public void Tick()
+        public async UniTaskVoid Drug()
         {
             var pos = Camera.main.transform.position;
-            Camera.main.transform.position = new Vector3(pos.x, pos.y, pos.z + speed);
-            if (pos.z / offset > counter)
-            {
-                counter++;
-                _pool.Despawn(_queue.Dequeue());
-                _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, offset * counter)));
-            }
+            await Camera.main.transform.DOMove(new Vector3(pos.x, pos.y, offset * _counter), 8).SetEase(Ease.Linear).ToUniTask();
+            _counter++;
+            _pool.Despawn(_queue.Dequeue());
+            _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, offset * _counter)));
+            Drug();
         }
     }
 }
