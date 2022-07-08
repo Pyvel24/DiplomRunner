f18e30213a42d7fe35570268e388375dd84e4fac
Pyvel24 <goloveyan@mail.ru>
2022-07-08 03:58:04 +0300

    Update GameInstaller.cs

--- DiplomRunner/Assets/Scripts/GameInstaller.cs
+++ DiplomRunner/Assets/Scripts/GameInstaller.cs
@@ -6,16 +6,14 @@ namespace DefaultNamespace
     public class GameInstaller: MonoInstaller
     {
         [SerializeField] private LevelView levelViewPrefab;
-        
+        [SerializeField] private Transform targetPrefab;
         public override void InstallBindings()
         {
             Container.BindMemoryPool<LevelView, LevelView.Pool>()
                 .WithInitialSize(2)
                 .FromNewComponentOnNewPrefab(levelViewPrefab);
-
+            Container.BindFactory<PlayerMovement, PlayerMovement.Factory>().FromNewComponentOnNewPrefab(targetPrefab);
             Container.BindInterfacesAndSelfTo<Test>().AsSingle().NonLazy();
         }
-        
-        
     }
 }
