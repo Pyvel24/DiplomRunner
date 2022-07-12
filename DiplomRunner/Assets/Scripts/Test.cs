ccd6f5cc273d299ae6baf9bc34ba62bfaf4aa1d9
Pyvel24 <goloveyan@mail.ru>
2022-07-13 00:24:30 +0300

    Update Test.cs

--- DiplomRunner/Assets/Scripts/Test.cs
+++ DiplomRunner/Assets/Scripts/Test.cs
@@ -2,37 +2,34 @@
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
-using Zenject;
 
-namespace DefaultNamespace
+public class Test 
 {
-    public class Test 
+    private int offset = 30;
+    private readonly LevelView.Pool _pool;
+    private int _counter = 1;
+    private Queue<LevelView> _queue;
+    private readonly PlayerMovement.Factory _factory;
+
+    public Test(LevelView.Pool pool, PlayerMovement.Factory factory)
     {
-        private int offset = 30;
-        private readonly LevelView.Pool _pool;
-        private int _counter = 1;
-        private Queue<LevelView> _queue;
-        
-        
-        public Test(LevelView.Pool pool)
-        {
-            _pool = pool;
-            _queue = new Queue<LevelView>();
-            _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, 0)));
-            _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, offset)));
-            Drug();
-        }
+        _pool = pool;
+        _factory = factory;
+        _factory.Create(new Vector3(0,1.6f,3.6f));
+        _queue = new Queue<LevelView>();
+        _queue.Enqueue(_pool.Spawn(new Vector3(0, 3.8f, 0)));
+        _queue.Enqueue(_pool.Spawn(new Vector3(0, 3.8f, offset)));
+        Drug();
+    }
         
 
-        public async UniTaskVoid Drug()
-        {
-            var pos = Camera.main.transform.position;
-            await Camera.main.transform.DOMove(new Vector3(pos.x, pos.y, offset * _counter), 8).SetEase(Ease.Linear).ToUniTask();
-            _counter++;
-            _pool.Despawn(_queue.Dequeue());
-            _queue.Enqueue(_pool.Spawn(new Vector3(0, 2, offset * _counter)));
-            Drug();
-        }
+    public async UniTaskVoid Drug()
+    {
+        var pos = Camera.main.transform.position;
+        await Camera.main.transform.DOMove(new Vector3(pos.x, pos.y, offset * _counter), 8).SetEase(Ease.Linear).ToUniTask();
+        _counter++;
+        _pool.Despawn(_queue.Dequeue());
+        _queue.Enqueue(_pool.Spawn(new Vector3(0, 3.8f, offset * _counter)));
+        Drug();
     }
-    
 }
