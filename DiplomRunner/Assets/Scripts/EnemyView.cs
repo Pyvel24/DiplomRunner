59067f8203ef84246e4b5728c23b9bd00e726c96
Pyvel24 <goloveyan@mail.ru>
2022-07-21 14:47:14 +0300

    add script EnemyView

---
+++ DiplomRunner/Assets/Scripts/EnemyView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks.Triggers;
+using DG.Tweening;
+using UnityEngine;
+using Zenject;
+using Random = UnityEngine.Random;
+
+public class EnemyView : MonoBehaviour
+{
+   
+    private void Update()
+    {
+        
+    }
+
+    public class Pool : MonoMemoryPool<Vector3, EnemyView>
+    {
+       
+        protected override void Reinitialize(Vector3 item, EnemyView enemyView)
+        {
+            enemyView.transform.position = item;
+        }
+    }
+}
