6003c5d8f965caba349bb654f4eb6210e34c9e1b
Pyvel24 <goloveyan@mail.ru>
2022-07-23 20:35:58 +0300

    Update EnemyView.cs

--- DiplomRunner/Assets/Scripts/EnemyView.cs
+++ DiplomRunner/Assets/Scripts/EnemyView.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks.Triggers;
+﻿
 using DG.Tweening;
 using UnityEngine;
 using Zenject;
-using Random = UnityEngine.Random;
 
 public class EnemyView : MonoBehaviour
 {
-   
-    private void Update()
-    {
-        
-    }
-
     public class Pool : MonoMemoryPool<Vector3, EnemyView>
     {
        
         protected override void Reinitialize(Vector3 item, EnemyView enemyView)
         {
             enemyView.transform.position = item;
+            enemyView.transform.DOMoveZ(item.z + 115, 60);
+        }
+
+        protected override void OnDespawned(EnemyView item)
+        {
+            item.gameObject.transform.DOKill();
+
+            base.OnDespawned(item);
+
         }
     }
 }
