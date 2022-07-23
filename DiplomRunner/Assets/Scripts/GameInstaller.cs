8d7b9aa735109527673d103609b9e204b633a55a
Pyvel24 <goloveyan@mail.ru>
2022-07-23 20:35:52 +0300

    Update GameInstaller.cs

--- DiplomRunner/Assets/Scripts/GameInstaller.cs
+++ DiplomRunner/Assets/Scripts/GameInstaller.cs
@@ -1,19 +1,25 @@
-﻿using UnityEngine;
+﻿using DefaultNamespace;
+using UnityEngine;
 using Zenject;
 
-namespace DefaultNamespace
+public class GameInstaller: MonoInstaller
 {
-    public class GameInstaller: MonoInstaller
+    [SerializeField] private LevelView levelViewPrefab;
+    [SerializeField] private PlayerMovement playerPrefab;
+    [SerializeField] private Transform mainLevels;
+    [SerializeField] private EnemyView enemyPrefab;
+    [SerializeField] private Transform enemyPrefabs;
+    public override void InstallBindings()
     {
-        [SerializeField] private LevelView levelViewPrefab;
-        [SerializeField] private Transform targetPrefab;
-        public override void InstallBindings()
-        {
-            Container.BindMemoryPool<LevelView, LevelView.Pool>()
-                .WithInitialSize(2)
-                .FromNewComponentOnNewPrefab(levelViewPrefab);
-            Container.BindFactory<PlayerMovement, PlayerMovement.Factory>().FromNewComponentOnNewPrefab(targetPrefab);
-            Container.BindInterfacesAndSelfTo<Test>().AsSingle().NonLazy();
-        }
+        Container.BindMemoryPool<LevelView, LevelView.Pool>()
+            .WithInitialSize(2)
+            .FromNewComponentOnNewPrefab(levelViewPrefab).UnderTransform(parent:mainLevels);
+        Container.BindInterfacesAndSelfTo<RoadGenerator>().AsSingle().NonLazy();
+        Container.BindFactory<Vector3, PlayerMovement, PlayerMovement.Factory>().AsSingle();
+        Container.Bind<PlayerMovement>().FromComponentInNewPrefab(playerPrefab).AsSingle();
+        Container.BindMemoryPool<EnemyView, EnemyView.Pool>().WithInitialSize(3)
+            .FromComponentInNewPrefab(enemyPrefab).UnderTransform(enemyPrefabs);
+        Container.BindInterfacesAndSelfTo<EnemyGenerator>().AsSingle().NonLazy();
+        Container.BindFactory<EnemyPrefabs, EnemyPrefabs.Factory>().AsSingle().NonLazy();
     }
 }
