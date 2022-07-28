e004e9c024e14af3bd85c35c55d145c1647feda5
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:49:28 +0300

    Create TaxiEnemyView script

---
+++ DiplomRunner/Assets/Scripts/TaxiEnemyView.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using Zenject;
+
+namespace Scripts
+{
+    public class TaxiEnemyView : MonoBehaviour
+    {
+        public class Pool : MonoMemoryPool<Vector3, TaxiEnemyView>
+        {
+            protected override void Reinitialize(Vector3 offset, TaxiEnemyView item)
+            {
+                item.transform.position = offset;
+                item.transform.DOMoveZ(offset.z + 115f, 60);
+            }
+
+            protected override void OnDespawned(TaxiEnemyView item)
+            {
+                item.gameObject.transform.DOKill();
+                base.OnDespawned(item);
+            }
+        }
+    }
+}
