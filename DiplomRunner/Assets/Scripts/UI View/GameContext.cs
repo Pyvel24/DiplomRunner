69deda0bd7988ec0ac175d1cfff5990fccac0471
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:48:51 +0300

    Create GameContext script

---
+++ DiplomRunner/Assets/Scripts/UI View/GameContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DefaultNamespace.Interfaces;
+using UI_View;
+using UnityEngine;
+
+namespace DefaultNamespace.UI_View
+{
+    public class GameContext : MonoBehaviour, IGameContext
+    {
+        public static GameContext Instance;
+        [SerializeField] private UIView[] views;
+        private UIView _currentView;
+        private void Start()
+        {
+            _currentView = views.First(v => v.ViewName == nameof(MainMenuUIView));
+            _currentView.Show();
+        }
+
+        public void ShowView(string viewName)
+        {
+            var tweener = _currentView.Hide();
+            tweener.onComplete += () =>
+            {
+                _currentView = views.First(v => v.ViewName == viewName);
+                _currentView.Show();
+            };
+        }
+
+        public void HideView()
+        {
+            _currentView.Hide();
+        }
+    }
+}
