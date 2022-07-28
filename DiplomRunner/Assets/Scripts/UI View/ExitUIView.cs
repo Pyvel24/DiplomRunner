ea12977bbafce721cb563df6642ca16688387170
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:49:01 +0300

    Create ExitUIView script

---
+++ DiplomRunner/Assets/Scripts/UI View/ExitUIView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DefaultNamespace.UI_View;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI_View
+{
+    public class ExitUIView : UIView
+    {
+        [SerializeField] private Button agreeButton;
+        [SerializeField] private Button disagreeButton;
+
+        private void Awake()
+        {
+            Initialize();
+            disagreeButton.onClick.AddListener((() => GameContext.Instance.ShowView(nameof(MainMenuUIView))));
+            agreeButton.onClick.AddListener(Application.Quit);
+        }
+
+        public override string ViewName => nameof(ExitUIView);
+    }
+}
