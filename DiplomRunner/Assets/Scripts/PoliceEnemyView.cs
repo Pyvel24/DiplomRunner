550a4f319878c32b0a7f2302ee08208b464f2c4d
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:50:25 +0300

    Create PoliceEnemyView script

---
+++ DiplomRunner/Assets/Scripts/PoliceEnemyView.cs
@@ -0,0 +1,24 @@
+﻿
+using DG.Tweening;
+using UnityEngine;
+using Zenject;
+
+public class PoliceEnemyView : MonoBehaviour
+{
+    public class Pool : MonoMemoryPool<Vector3, PoliceEnemyView>
+    {
+       
+        protected override void Reinitialize(Vector3 item, PoliceEnemyView policeEnemyView)
+        {
+            policeEnemyView.transform.position = item;
+            policeEnemyView.transform.DOMoveZ(item.z + 115, 60);
+        }
+
+        protected override void OnDespawned(PoliceEnemyView item)
+        {
+            item.gameObject.transform.DOKill();
+
+            base.OnDespawned(item);
+        }
+    }
+}
