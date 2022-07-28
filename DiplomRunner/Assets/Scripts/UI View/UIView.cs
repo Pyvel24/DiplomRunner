e8debd2a270c19dc90c1f6d27ce93b5b1465feae
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:47:55 +0300

    create UIView script

---
+++ DiplomRunner/Assets/Scripts/UI View/UIView.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace UI_View
+{
+    public abstract  class UIView : MonoBehaviour
+    {
+        public abstract string ViewName { get;  }
+
+        protected void Initialize()
+        {
+            transform.localScale=Vector3.zero;
+        }
+
+        public Tweener Show()
+        {
+            gameObject.SetActive(true);
+            return transform.DOScale(1f, .3f);
+            
+        }
+
+        public Tweener Hide()
+        {
+            var tweener = transform.DOScale(0f, .5f);
+            tweener.onComplete += () => gameObject.SetActive(false);
+            return tweener;
+        }
+    }
+}
