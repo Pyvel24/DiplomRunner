5a11cf5ed6545951d5770df6fb09b9097409f71b
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:52:59 +0300

    Create CarEnemyView script

---
+++ DiplomRunner/Assets/Scripts/CarEnemyView.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using Scripts;
+using UnityEngine;
+using Zenject;
+
+namespace Scripts
+{
+    public class CarEnemyView : MonoBehaviour
+    {
+        public class Pool : MonoMemoryPool<Vector3, CarEnemyView>
+        {
+            protected override void Reinitialize(Vector3 offset, CarEnemyView item)
+            {
+                item.transform.position = offset;
+                item.transform.DOMoveZ(offset.z + 115f, 60);
+            }
+
+            protected override void OnDespawned(CarEnemyView item)
+            {
+                item.gameObject.transform.DOKill();
+                base.OnDespawned(item);
+            }
+        }
+    }
+}
