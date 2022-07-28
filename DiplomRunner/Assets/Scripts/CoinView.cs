6a50a31ecddce5fd315fb184791d682d4dc23e87
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:53:25 +0300

    Create CoinView script

---
+++ DiplomRunner/Assets/Scripts/CoinView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace
+{
+    public class CoinView: MonoBehaviour
+    {
+        private float rotationSpeed = 100;
+       
+        private void Start()
+        {
+            rotationSpeed += Random.Range(0, rotationSpeed / 4.0f);
+        }
+
+        private void Update()
+        {
+            transform.Rotate(0,0,rotationSpeed*Time.deltaTime);
+        }
+        
+        
+        public class Pool : MonoMemoryPool<Vector3, CoinView>
+        {
+            protected override void Reinitialize(Vector3 p1, CoinView item)
+            {
+                item.transform.position = p1;
+            }
+           
+        }
+    }
+}
