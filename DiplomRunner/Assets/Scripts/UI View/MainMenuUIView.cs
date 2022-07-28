929717b8bfb6ff3e3a46ad74ed564f5121ec6356
Pyvel24 <goloveyan@mail.ru>
2022-07-29 01:48:16 +0300

    Create MainMenuUIView

---
+++ DiplomRunner/Assets/Scripts/UI View/MainMenuUIView.cs
@@ -0,0 +1,22 @@
+﻿using DefaultNamespace.UI_View;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI_View
+{
+    public class MainMenuUIView : UIView
+    {
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button exitButton;
+        [SerializeField] private Button settingButton;
+
+        private void Awake()
+        {
+            Initialize();
+            exitButton.onClick.AddListener((() => GameContext.Instance.ShowView(nameof(ExitUIView))));
+            settingButton.onClick.AddListener((() => GameContext.Instance.ShowView(nameof(SettingUIView))));
+        }
+
+        public override string ViewName => nameof(MainMenuUIView);
+    }
+}
