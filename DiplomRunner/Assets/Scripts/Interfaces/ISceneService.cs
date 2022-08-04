6dcee5cb8258e028e3b977dd7f0bd1a08ddb03b9
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:49:21 +0300

    Create ISceneService.cs

---
+++ DiplomRunner/Assets/Scripts/Interfaces/ISceneService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace.Interfaces
+{
+    public interface ISceneService
+    {
+        AsyncOperation LoadScene(string name);
+        AsyncOperation UnLoadScene(string name);
+        bool IsLoading { get; }
+        IEnumerable<GameObject> GetActiveRoots();
+    }
+}
