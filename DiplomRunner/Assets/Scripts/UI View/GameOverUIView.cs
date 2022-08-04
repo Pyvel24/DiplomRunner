8a64bd4b2e1c0838e0eb692fe3eae881037bb4a4
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:48:21 +0300

    Update GameOverUIView.cs

--- DiplomRunner/Assets/Scripts/UI View/GameOverUIView.cs
+++ DiplomRunner/Assets/Scripts/UI View/GameOverUIView.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
-namespace DefaultNamespace.UI_View
+namespace UI_View
 {
-    public class GameOverUIView : MonoBehaviour
+    public class GameOverUIView : UIView
     {
-        
+        [SerializeField] private Button homeButton;
+        [SerializeField] private Button replayButton;
+        private void Awake()
+        {
+            homeButton.onClick.AddListener(() => GameContext.Instance.ShowView(nameof(MainMenuUIView)));
+            replayButton.onClick.AddListener(Restart);
+            Initialize();
+        }
+        private void Restart()
+        {
+            var asyncOperation = GameContext.Instance.SceneService.LoadScene("MainScene");
+            asyncOperation.completed += operation =>
+            {
+                GameContext.Instance.HideView();
+            };
+            
+        }
+
+        public override string ViewName => nameof(GameOverUIView);
     }
 }
