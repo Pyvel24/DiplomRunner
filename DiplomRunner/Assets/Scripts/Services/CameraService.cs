8b5d4f2f07a2c212bcc479504f6933877f844e5a
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:48:55 +0300

    Create CameraService.cs

---
+++ DiplomRunner/Assets/Scripts/Services/CameraService.cs
@@ -0,0 +1,29 @@
+﻿using DefaultNamespace.Interfaces;
+using UnityEngine;
+
+namespace Services
+{
+    public class CameraService: ICameraService
+    {
+        private readonly Camera _defaultCamera;
+
+        private Camera _currentCamera;
+
+        public CameraService(Camera defaultCamera)
+        {
+            _defaultCamera = defaultCamera;
+            _currentCamera = defaultCamera;
+        }
+        public void SetMainCamera(Camera camera)
+        {
+            _currentCamera.enabled = false;
+            camera.enabled = true;
+            _currentCamera = camera;
+        }
+
+        public void SetDefaultCamera()
+        {
+            SetMainCamera(_defaultCamera);
+        }
+    }
+}
