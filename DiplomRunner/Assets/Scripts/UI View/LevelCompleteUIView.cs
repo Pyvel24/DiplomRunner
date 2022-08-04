9675884976b06721b09a50062178a018cb1ea243
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:48:19 +0300

    Update LevelCompleteUIView.cs

--- DiplomRunner/Assets/Scripts/UI View/LevelCompleteUIView.cs
+++ DiplomRunner/Assets/Scripts/UI View/LevelCompleteUIView.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-namespace DefaultNamespace.UI_View
+namespace UI_View
 {
-    public class LevelCompleteUIView : MonoBehaviour
-    {
+    public class LevelCompleteUIView : UIView
+    {   [SerializeField] private Button homeButton;
+        [SerializeField] private Button replayButton;
+        private void Awake()
+        {
+            homeButton.onClick.AddListener(() => GameContext.Instance.ShowView(nameof(MainMenuUIView)));
+            replayButton.onClick.AddListener(Restart);
+            Initialize();
+        }
+        private void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Time.timeScale = 1f;
+        }
         
+        public override string ViewName => nameof(LevelCompleteUIView);
     }
 }
