7e726c3b637ca89e6e5266f6a168a2c80ecdb124
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:49:18 +0300

    Create ISaveService.cs

---
+++ DiplomRunner/Assets/Scripts/Interfaces/ISaveService.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultNamespace.Interfaces
+{
+    public interface ISaveService
+    {
+        void Write(object obj, string name);
+        T Load<T>(string name);
+        void Save ();
+    }
+}
