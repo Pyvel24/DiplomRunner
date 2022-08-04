7751b8b016dba0aba042c11ed97fd2d6efa5a08f
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:48:46 +0300

    Update GameContext.cs

--- DiplomRunner/Assets/Scripts/UI View/GameContext.cs
+++ DiplomRunner/Assets/Scripts/UI View/GameContext.cs
@@ -1,20 +1,38 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using DefaultNamespace.Interfaces;
+using Models;
+using Services;
 using UI_View;
 using UnityEngine;
 
-namespace DefaultNamespace.UI_View
+namespace UI_View
 {
     public class GameContext : MonoBehaviour, IGameContext
     {
         public static GameContext Instance;
         [SerializeField] private UIView[] views;
         private UIView _currentView;
+        public ISceneService SceneService { get; private set; }
+        public ISaveService SaveService { get; private set; }
+        public ICameraService CameraService { get; set; }
+        private void Awake()
+        {
+            SceneService = new SceneService();
+            SaveService = new SaveService();
+            CameraService = new CameraService(Camera.main);
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            CheckModels();
+        }
         private void Start()
         {
             _currentView = views.First(v => v.ViewName == nameof(MainMenuUIView));
             _currentView.Show();
         }
+        
 
         public void ShowView(string viewName)
         {
@@ -29,6 +47,16 @@ namespace DefaultNamespace.UI_View
         public void HideView()
         {
             _currentView.Hide();
+        }
+        private void CheckModels()
+        {
+            var pm = SaveService.Load<ProgressModel>();
+            if (pm == null)
+            {
+                pm = new ProgressModel();
+                SaveService.Write(pm);
+            }
+           
         }
     }
 }
