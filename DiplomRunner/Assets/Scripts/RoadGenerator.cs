66b0f046463abb311a84d4339a7d616bde9b6e5e
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:47:00 +0300

    Update RoadGenerator.cs

--- DiplomRunner/Assets/Scripts/RoadGenerator.cs
+++ DiplomRunner/Assets/Scripts/RoadGenerator.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
 
-public class RoadGenerator 
+public class RoadGenerator: IDisposable 
 {
     private int offset = 115;
     private readonly LevelView.Pool _pool;
     private int _counter = 1;
     private Queue<LevelView> _roadQueue;
     private readonly PlayerMovement.Factory _factory;
-
+    private readonly CancellationTokenSource _cts;
     public RoadGenerator(LevelView.Pool pool, PlayerMovement.Factory factory)
     {
         _pool = pool;
@@ -19,17 +21,28 @@ public class RoadGenerator
         _roadQueue = new Queue<LevelView>();
         _roadQueue.Enqueue(_pool.Spawn(new Vector3(0, 3.8f, 0)));
         _roadQueue.Enqueue(_pool.Spawn(new Vector3(0, 3.8f, offset)));
-        Drug();
+        _cts = new CancellationTokenSource();
+        Drug(_cts.Token);
     }
     
-    public async UniTaskVoid Drug()
+    public async UniTaskVoid Drug(CancellationToken cts)
     {
+        
         var pos = Camera.main.transform.position;
         await Camera.main.transform.DOMove(new Vector3(pos.x, pos.y, offset * _counter), 20).SetEase(Ease.Linear).ToUniTask();
         _counter++;
-        _pool.Despawn(_roadQueue.Dequeue());
+        var roadQueue = _roadQueue.Dequeue();
+        if (cts.IsCancellationRequested)
+        {
+            return;
+        }
+        _pool.Despawn(roadQueue);
         _roadQueue.Enqueue(_pool.Spawn(new Vector3(0, 3.8f, offset * _counter)));
-        Drug();
+        Drug(cts);
     }
-    
+
+    public void Dispose()
+    {
+        _cts.Cancel();
+    }
 }
