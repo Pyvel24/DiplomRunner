6f313512daa1780005b1f339152c4053c09d7a1c
Pyvel24 <goloveyan@mail.ru>
2022-08-05 00:48:42 +0300

    Create SceneService script

---
+++ DiplomRunner/Assets/Scripts/Services/SceneService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace.Interfaces;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Services
+{
+    public class SceneService: ISceneService
+    {
+        private Scene _currentScene;
+        private ISceneService _sceneServiceImplementation;
+
+        public SceneService()
+        {
+            _currentScene = SceneManager.GetActiveScene();
+        }
+
+        public AsyncOperation LoadScene(string name)
+        {
+            var asyncOperation = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+            IsLoading = true;
+            asyncOperation.completed += operation =>
+            {
+                var scene = SceneManager.GetSceneByName(name);
+                
+                SceneManager.SetActiveScene(scene);
+                _currentScene = scene;
+                IsLoading = false;
+            };
+            return asyncOperation;
+        }
+
+        public AsyncOperation UnLoadScene(string name)
+        {
+            return SceneManager.UnloadSceneAsync(name);
+        }
+
+        public bool IsLoading { get; private set; }
+        public IEnumerable<GameObject> GetActiveRoots()
+        {
+            return _currentScene.GetRootGameObjects();
+        }
+    }
+}
