c00fce15a4f6add39a91d7f6f36abbfb0fe8b3b5
Pyvel24 <goloveyan@mail.ru>
2022-08-16 15:50:58 +0300

    Update GameContext.cs

--- DiplomRunner/Assets/Scripts/UI View/GameContext.cs
+++ DiplomRunner/Assets/Scripts/UI View/GameContext.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Linq;
 using DefaultNamespace.Interfaces;
+using Interfaces;
 using Models;
 using Services;
 using UI_View;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace UI_View
 {
@@ -12,27 +14,44 @@ namespace UI_View
     {
         public static GameContext Instance;
         [SerializeField] private UIView[] views;
+        [SerializeField] private AudioService.Sound[] sounds;
         private UIView _currentView;
         public ISceneService SceneService { get; private set; }
         public ISaveService SaveService { get; private set; }
         public ICameraService CameraService { get; set; }
+        public IAudioService AudioService { get; private set; }
         private void Awake()
         {
+            foreach (var sound in sounds)
+            {
+                sound.source = gameObject.AddComponent<AudioSource>();
+                sound.source.clip = sound.clip;
+                sound.source.loop = sound.loop;
+            }
+            AudioService = new AudioService(sounds);
             SceneService = new SceneService();
             SaveService = new SaveService();
             CameraService = new CameraService(Camera.main);
+            
+            
             if (Instance == null)
             {
                 Instance = this;
             }
             CheckModels();
+            
         }
+        
+
         private void Start()
         {
             _currentView = views.First(v => v.ViewName == nameof(MainMenuUIView));
             _currentView.Show();
+            AudioService.Play("Main");
+            
         }
         
+
 
         public void ShowView(string viewName)
         {
@@ -55,6 +74,12 @@ namespace UI_View
             {
                 pm = new ProgressModel();
                 SaveService.Write(pm);
+            }
+            var s = SaveService.Load<SettingModel>();
+            if (s == null)
+            {
+                s = new SettingModel();
+                SaveService.Write(s);
             }
            
         }
