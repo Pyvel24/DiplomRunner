54552ea523237ff98d043c415a43734109232a37
Pyvel24 <goloveyan@mail.ru>
2022-08-16 15:54:18 +0300

    Update PlayerMovement.cs

--- DiplomRunner/Assets/Scripts/PlayerMovement.cs
+++ DiplomRunner/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using DefaultNamespace;
 using DG.Tweening;
 using Scripts;
+using Services;
 using Signal;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,20 +15,27 @@ public class PlayerMovement: MonoBehaviour
     [Inject] private SignalBus _signalBus;
     [Inject] private CoinView.Pool _pool;
     [Inject] private SignalBus _signal;
-   
+    [SerializeField] private AudioSource moveLeft;
+    [SerializeField] private AudioSource moveRight;
+    [SerializeField] private AudioSource moveCenter;
+    [SerializeField] private AudioSource coin;
+    [SerializeField] private AudioSource damage;
     private void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.DOMoveX(-0.94f, 1.2f);
+            moveLeft.Play();
         }
         else if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.DOMoveX(-0.01f, 1.2f);
+            moveCenter.Play();
         }
         else if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.DOMoveX(0.94f, 1.2f);
+            moveRight.Play();
         }
         
         
@@ -37,12 +45,14 @@ public class PlayerMovement: MonoBehaviour
         if (other.TryGetComponent<CoinView>(out var coin))
         {
             _signalBus.Fire<CoinCollected>();
+            this.coin.Play();
             other.gameObject.SetActive(false);
         }
 
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Obstacle"))
         {
             _signal.Fire<Health>();
+            damage.Play();
         }
         
     }
