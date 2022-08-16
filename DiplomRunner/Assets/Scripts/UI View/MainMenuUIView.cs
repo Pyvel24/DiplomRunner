027c32166761e686f69a60a1d1375f960cf11af6
Pyvel24 <goloveyan@mail.ru>
2022-08-16 15:50:51 +0300

    Update MainMenuUIView.cs

--- DiplomRunner/Assets/Scripts/UI View/MainMenuUIView.cs
+++ DiplomRunner/Assets/Scripts/UI View/MainMenuUIView.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using DefaultNamespace.UI_View;
+using DG.Tweening;
+using Interfaces;
 using Models;
+using Services;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,21 +16,47 @@ namespace UI_View
         [SerializeField] private Button startButton;
         [SerializeField] private Button exitButton;
         [SerializeField] private Button settingButton;
+        [SerializeField] private Button messageButton;
+        [SerializeField] private Button missionButton;
+        [SerializeField] private AudioSource clickToGame;
+        [SerializeField] private AudioSource clickToExit;
+        [SerializeField] private AudioSource clickToSettings;
+        [SerializeField] private AudioSource clickToMessage;
+        [SerializeField] private AudioSource clickToMission;
         
-
         private void Awake()
         {
             Initialize();
-            exitButton.onClick.AddListener(() => GameContext.Instance.ShowView(nameof(ExitUIView)));
-            settingButton.onClick.AddListener(() => GameContext.Instance.ShowView(nameof(SettingUIView)));
             startButton.onClick.AddListener(() =>
             {
+                clickToGame.Play();
+                
                 var asyncOperation = GameContext.Instance.SceneService.LoadScene("MainScene");
                 asyncOperation.completed += operation =>
                 {
                     GameContext.Instance.HideView();
                 };
             });
+            exitButton.onClick.AddListener(() =>
+            {
+                GameContext.Instance.ShowView(nameof(ExitUIView));
+                clickToExit.Play();
+            });
+            settingButton.onClick.AddListener(() =>
+            {
+                GameContext.Instance.ShowView(nameof(SettingUIView));
+                clickToSettings.Play();
+            });
+            messageButton.onClick.AddListener((() =>
+            {
+                GameContext.Instance.ShowView(nameof(MessageUI));
+                clickToMessage.Play();
+            }));
+            missionButton.onClick.AddListener((() =>
+            {
+                GameContext.Instance.ShowView(nameof(MissionUI));
+                clickToMission.Play();
+            }));
         }
 
         public override string ViewName => nameof(MainMenuUIView);
