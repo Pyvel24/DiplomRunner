4cec219803a6fc3c3a781666fda05fe06409593c
Pyvel24 <goloveyan@mail.ru>
2022-08-16 15:50:34 +0300

    Add MisiionUI script

---
+++ DiplomRunner/Assets/Scripts/UI View/MissionUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI_View
+{
+    public class MissionUI :UIView
+    {
+        [SerializeField] private Button exitButton;
+        [SerializeField] private AudioSource clickToExit;
+        private void Awake()
+        {
+            exitButton.onClick.AddListener((() =>
+            {
+                GameContext.Instance.ShowView(nameof(MainMenuUIView));
+                clickToExit.Play();
+            }));
+            Initialize();
+        }
+        public override string ViewName => nameof(MissionUI);
+    }
+}
