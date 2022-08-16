bbf8b963347c9f27d44b602a9aa0eb03421f4a49
Pyvel24 <goloveyan@mail.ru>
2022-08-16 15:54:11 +0300

    Update GameInstaller.cs

--- DiplomRunner/Assets/Scripts/GameInstaller.cs
+++ DiplomRunner/Assets/Scripts/GameInstaller.cs
@@ -1,25 +1,49 @@
 ﻿using DefaultNamespace;
+using Signal;
 using UnityEngine;
 using Zenject;
 
-public class GameInstaller: MonoInstaller
+namespace Scripts
 {
-    [SerializeField] private LevelView levelViewPrefab;
-    [SerializeField] private PlayerMovement playerPrefab;
-    [SerializeField] private Transform mainLevels;
-    [SerializeField] private EnemyView enemyPrefab;
-    [SerializeField] private Transform enemyPrefabs;
-    public override void InstallBindings()
+    public class GameInstaller : MonoInstaller
     {
-        Container.BindMemoryPool<LevelView, LevelView.Pool>()
-            .WithInitialSize(2)
-            .FromNewComponentOnNewPrefab(levelViewPrefab).UnderTransform(parent:mainLevels);
-        Container.BindInterfacesAndSelfTo<RoadGenerator>().AsSingle().NonLazy();
-        Container.BindFactory<Vector3, PlayerMovement, PlayerMovement.Factory>().AsSingle();
-        Container.Bind<PlayerMovement>().FromComponentInNewPrefab(playerPrefab).AsSingle();
-        Container.BindMemoryPool<EnemyView, EnemyView.Pool>().WithInitialSize(3)
-            .FromComponentInNewPrefab(enemyPrefab).UnderTransform(enemyPrefabs);
-        Container.BindInterfacesAndSelfTo<EnemyGenerator>().AsSingle().NonLazy();
-        Container.BindFactory<EnemyPrefabs, EnemyPrefabs.Factory>().AsSingle().NonLazy();
+        [SerializeField] private LevelView levelViewPrefab;
+        [SerializeField] private PlayerMovement playerPrefab;
+        [SerializeField] private Transform mainLevels;
+        [SerializeField] private PoliceEnemyView copPrefab;
+        [SerializeField] private Transform enemyPrefabs;
+        [SerializeField] private CoinView coinPrefab;
+        [SerializeField] private Transform coinPrefabs;
+        [SerializeField] private TaxiEnemyView taxiPrefab;
+        [SerializeField] private CarEnemyView carPrefab;
+        [SerializeField] private LevelUiView levelUiView;
+
+        public override void InstallBindings()
+        {
+            SignalBusInstaller.Install(Container);
+
+            Container.DeclareSignal<CoinCollected>();
+            Container.DeclareSignal<Health>();
+            Container.BindMemoryPool<LevelView, LevelView.Pool>()
+                .WithInitialSize(2)
+                .FromNewComponentOnNewPrefab(levelViewPrefab).UnderTransform(parent: mainLevels);
+            Container.BindInterfacesAndSelfTo<RoadGenerator>().AsSingle().NonLazy();
+            Container.BindFactory<Vector3, PlayerMovement, PlayerMovement.Factory>().AsSingle();
+            Container.Bind<PlayerMovement>().FromComponentInNewPrefab(playerPrefab).AsSingle();
+            Container.BindMemoryPool<PoliceEnemyView, PoliceEnemyView.Pool>().WithInitialSize(3)
+                .FromNewComponentOnNewPrefab(copPrefab).UnderTransform(enemyPrefabs);
+            Container.BindInterfacesAndSelfTo<PoliceEnemyGenerator>().AsSingle().NonLazy();
+            Container.BindMemoryPool<TaxiEnemyView, TaxiEnemyView.Pool>().WithInitialSize(3)
+                .FromNewComponentOnNewPrefab(taxiPrefab).UnderTransform(enemyPrefabs);
+            Container.BindInterfacesAndSelfTo<TaxiEnemyGenerator>().AsSingle().NonLazy();
+            Container.BindMemoryPool<CoinView, CoinView.Pool>().WithInitialSize(15).FromComponentInNewPrefab(coinPrefab)
+                .UnderTransform(coinPrefabs);
+            Container.BindInterfacesAndSelfTo<CoinGenerator>().AsSingle().NonLazy();
+            Container.BindMemoryPool<CarEnemyView, CarEnemyView.Pool>().WithInitialSize(3)
+                .FromNewComponentOnNewPrefab(carPrefab).UnderTransform(enemyPrefabs);
+            Container.BindInterfacesAndSelfTo<CarEnemyGenerator>().AsSingle().NonLazy();
+            Container.Bind<LevelUiView>().FromInstance(levelUiView).AsSingle();
+ 
+        }
     }
 }
